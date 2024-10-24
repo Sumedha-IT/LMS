6c27c26e66cbe27b19ba0582db9fc131dca54e87
Paul Fansare <paul.f@sumedhait.com>
2024-10-24 19:27:48 +0530

    paul | resolved conflict

--- database/migrations/2024_10_24_133804_add_zoho_customer_id_to_user_table.php
+++ database/migrations/2024_10_24_133804_add_zoho_customer_id_to_user_table.php
@@ -25,12 +25,4 @@ return new class extends Migration
             $table->dropColumn('zoho_crm_id'); // Remove the column if rolling back
         });
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> b647374f1708982fdbe3aee44ffd6e7cbf18936e
-=======
-};
->>>>>>> 7c30c59638a9c12ae28a36a478aaa51305c6f9d5
