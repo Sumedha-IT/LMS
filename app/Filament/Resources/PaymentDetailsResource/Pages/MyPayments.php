23ab2d819d7fdbe22edce596b47e6ae9a25ce7e2
Paul Fansare <paul.f@sumedhait.com>
2024-11-06 10:41:18 +0530

    Added Css to the feature

--- app/Filament/Resources/PaymentDetailsResource/Pages/MyPayments.php
+++ app/Filament/Resources/PaymentDetailsResource/Pages/MyPayments.php
@@ -4,10 +4,20 @@ namespace App\Filament\Resources\PaymentDetailsResource\Pages;
 
 use App\Filament\Resources\PaymentDetailsResource;
 use Filament\Resources\Pages\Page;
+use Illuminate\Support\Facades\Auth;
 
 class MyPayments extends Page
 {
     protected static string $resource = PaymentDetailsResource::class;
 
     protected static string $view = 'filament.resources.payment-details-resource.pages.my-payments';
+    publiC $disableInteraction ;
+
+
+    public function mount()
+    {   
+        $user = Auth::user();
+        $this->disableInteraction = empty($user) ? false : ($user->feature_access == true ? false : true);
+    }
+
 }
--- resources/views/filament/pages/payment-details-dashboard.blade.php
+++ resources/views/filament/pages/payment-details-dashboard.blade.php
@@ -1,3 +1,10 @@
 <x-filament-panels::page>
 
+<div class="fi-sidebar-item"
+     @if($disableInteraction) 
+         style="pointer-events: none; color: lightgray; opacity: 0.6;" 
+     @endif>
+    Sidebar Item
+</div>
+
 </x-filament-panels::page>
