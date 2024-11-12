ea0b1e2f954093fc128749603a475b381ffbc3c4
Paul Fansare <paul.f@sumedhait.com>
2024-11-12 12:50:09 +0530

    Css issue bug fix for feedback form

--- resources/views/components/iframe.blade.php
+++ resources/views/components/iframe.blade.php
@@ -1,6 +1,8 @@
-
-<x-filament-panels::page>
-<iframe id="myIframe" src="{{ $src }}" width="100%" height="600px" frameborder="0" allowfullscreen
-></iframe>
-
-</x-filament-panels::page>
+<div>
+    <iframe src="{{ $src }}" width="100%" height="600px" frameborder="0" allowfullscreen></iframe>
+    <style>
+        .fi-modal-footer-actions {
+            display: none;
+        }
+    </style>
+</div>
