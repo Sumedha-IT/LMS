a0a39045ff36b652ad518cec87a51346231b7acb
Paul Fansare <paul.f@sumedhait.com>
2024-11-29 13:06:32 +0530

    harsh| disable token deletion

--- app/Listeners/OnLoginEventListener.php
+++ app/Listeners/OnLoginEventListener.php
@@ -25,9 +25,9 @@ class OnLoginEventListener
     {
         $user = $event->user;
         
-        if($user->tokens()->count() > 0){
-            $user->tokens()->delete();
-        }
+        // if($user->tokens()->count() > 0){
+        //     $user->tokens()->delete();
+        // }
 
         // Create a new token for the user
         $token = $user->createToken('app_token');
