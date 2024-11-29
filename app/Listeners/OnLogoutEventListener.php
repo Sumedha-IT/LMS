b4ea4fab01364441b43ffe0a2d74ab8e1adcc0d6
Paul Fansare <paul.f@sumedhait.com>
2024-11-29 13:21:33 +0530

    disabke single token deletion on logout

--- app/Listeners/OnLogoutEventListener.php
+++ app/Listeners/OnLogoutEventListener.php
@@ -21,7 +21,7 @@ class OnLogoutEventListener
     public function handle(object $event): void
     {
         if($event->user){
-            $event->user->token()->delete();
+            $event->user->tokens()->delete();
         }
         setcookie('x_path_id', "", time() - (24 * 60 * 60 * 60), "/");
         setcookie('user_info', "", time() - (24 * 60 * 60 * 60), "/"); 
